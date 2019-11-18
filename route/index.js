6b91a7c4c3ba94dfd0173594c9de0c7627a612a6
slowlife7 <rasgo@naver.com>
2019-11-18 17:40:43 +0900

    인덱스

--- route/index.js
+++ route/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const calendar = require("../public/javascript/calendar")();
+const record = require("../model/record");
+const moment = require("moment");
 
 router.get("/", (req, res, next) => {
   const session = req.session;
@@ -16,7 +18,40 @@ router.get("/", (req, res, next) => {
       month: date.getMonth() + 1
     });
 
+<<<<<<< Updated upstream
     res.render("calendar", { title: dateString, days: cal.days, start:cal.startIndex, end:cal.days.length - 1 });
+=======
+    console.log(cal.days);
+
+    const start = `"${dateString}.${cal.days[cal.startIndex]}"`;
+    const end = `"${dateString}.${cal.days[cal.days.length - 1]}"`;
+    console.log(start);
+    console.log(end);
+
+    record
+      .find({
+        userid: session.userid,
+        conductedAt: { $gte: new Date(start), $lt: new Date(end) }
+      })
+      .then(result => {
+        console.log(result);
+
+        const completedDays = [];
+        if (Array.isArray(result)) {
+          result.forEach(item => {
+            const day = moment(item.conductedAt).format("DD");
+            completedDays.push(day);
+          });
+        }
+
+        console.dir(completedDays);
+        res.render("calendar", {
+          title: dateString,
+          days: cal.days,
+          complete: completedDays
+        });
+      });
+>>>>>>> Stashed changes
   }
 });
 
